4960fb29db06ef55c5ec2998a9b289e8b13f0515
Callan Hoskins <chosk@stanford.edu>
2018-06-04 18:03:14 -0700

    Setup for sender module

---
+++ sender.c
@@ -0,0 +1,12 @@
+#include "gpio.h"
+#include "gpioextra.h"
+#include "timer.h"
+#include "sender.h"
+
+void send_code(unsigned int pin, unsigned int code, unsigned int BAUD_RATE) {
+    
+}
+
+void read_code(unsigned int pin, unsigned int BAUD_RATE) {
+
+}
---
+++ sender.h
@@ -0,0 +1,30 @@
+#ifndef SENDER_H
+#define SENDER_H
+
+/*
+ * Functions for sending scancodes from the Raspberry Pi
+ *
+ * Author: Callan Hoskins, Gen Singer, Maika Isogawa
+ * 4 June 2018
+ */
+
+/*
+ * function: sender_init
+ * Initializes global variables
+ * @param tx_pin - pin that should be used to transmit
+ * @param rx_pin - pin that should be read from
+ */
+void sender_init(unsigned int tx_pin, unsigned int rx_pin, 
+    unsigned int baud_rate);
+/*
+ * Send code from pin using gpio_write
+ */
+void sender_send_code(unsigned int code);
+
+/*
+ * Read code from pin using gpio_read
+ */
+
+void sender_read_code(void);
+
+#endif
