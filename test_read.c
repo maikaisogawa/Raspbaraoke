346ee1138fac61d92e778728c2c1fc16cd9b1168
Genevieve Singer <gsinger@stanford.edu>
2018-06-05 14:29:42 -0700

    test led

--- test_read.c
+++ test_read.c
@@ -1,13 +1,13 @@
-#include sender.h
-#include gpio.h
-#include gpioextra.h
-#include printf.h
+#include "sender.h"
+#include "gpio.h"
+#include "gpioextra.h"
+#include "printf.h"
 
 static unsigned long long TEST_BYTE = 0xFF;
 
-main() {
+void main() {
     sender_init(GPIO_PIN23, GPIO_PIN24, 9600);
-    TEST_BYTE = sender_read_scancode();
+    TEST_BYTE = sender_read_code();
     printf("the scancode is: %x", (unsigned int)TEST_BYTE);
 }
  
