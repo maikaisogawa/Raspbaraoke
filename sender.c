e014c56450e51e95569795058e8043813a540cc1
Callan Hoskins <chosk@stanford.edu>
2018-06-04 18:08:06 -0700

    Set up the sender module

--- sender.c
+++ sender.c
@@ -3,10 +3,22 @@
 #include "timer.h"
 #include "sender.h"
 
-void send_code(unsigned int pin, unsigned int code, unsigned int BAUD_RATE) {
-    
+static const unsigned int TX_PIN, RX_PIN, BAUD_RATE; 
+
+void sender_init(unsigned int tx_pin, unsigned int rx_pin, 
+    unsigned int baud_rate) {
+    RX_PIN = rx_pin;
+    TX_PIN = tx_pin;
+    BAUD_RATE = baud_rate;
 }
 
-void read_code(unsigned int pin, unsigned int BAUD_RATE) {
+void send_code(unsigned int code) {
+    double DELAY_US = (1.0/BAUD_RATE)*1000000.0;
+    while (code) {
+        unsigned int scancode = code & 0xff;
+        code >> 8;
+}
+
+unsigned int read_code(void) {
 
 }
