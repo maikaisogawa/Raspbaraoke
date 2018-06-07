90ff1574fc4fc942fb63da30151ca12f10486b43
Genevieve Singer <gsinger@stanford.edu>
2018-06-06 19:29:19 -0700

    songs.h

---
+++ songs.h
@@ -0,0 +1,13 @@
+#define NUM_SONGS 6
+
+const char * titles[NUM_SONGS];
+
+void load_titles() 
+{
+    titles[0] = "After the Storm";
+    titles[1] = "Boo'd Up";
+    titles[2] = "In The Morning";
+    titles[3] = "Season 2 Episode 3";
+    titles[4] = "Honey";
+    titles[5] = "Total";
+}
