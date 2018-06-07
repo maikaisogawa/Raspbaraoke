8cebb21cc4386719cbad94c2b46ec645d5631490
Callan Hoskins <chosk@stanford.edu>
2018-06-06 19:17:20 -0700

    cleaned up tests

--- tests/test_send.c
+++ tests/test_send.c
@@ -3,15 +3,10 @@
 #include "gpioextra.h"
 #include "printf.h"
 
-static unsigned long long TEST_BYTE = 0x7e0541000045ef;
+static unsigned long long TEST_BYTE = 0x7effef;
 
-void main(void) {
-    gpio_set_input(GPIO_PIN24);
-    gpio_set_pulldown(GPIO_PIN24);
-    printf("before init, reading: %x\n", gpio_read(GPIO_PIN24));
+void main() {
     sender_init(GPIO_PIN23, GPIO_PIN24, 9600);
-    printf("after init, reading: %x\n", gpio_read(GPIO_PIN24));
-    sender_send_code(TEST_BYTE);
-    printf("after sending code, reading: %x\n", gpio_read(GPIO_PIN24));
+    sender_send_scancode(TEST_BYTE);
 }
     
