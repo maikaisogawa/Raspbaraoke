470c4e3568cf1c4b4cd09f7384895feb1b70bac6
Callan Hoskins <chosk@stanford.edu>
2018-06-06 23:41:09 -0700

    Better test

--- tests/test_send.c
+++ tests/test_send.c
@@ -2,11 +2,22 @@
 #include "gpio.h"
 #include "gpioextra.h"
 #include "printf.h"
+#include "timer.h"
 
-static unsigned long long TEST_BYTE = 0x7effef;
+static unsigned long long TEST_BYTE = 0x7e030102ef;
+#define PLAY 0x7E030102EF
+#define STOP 0x7E030E0DEF
+#define SETUP 0x7E0541000145EF
 
 void main() {
     sender_init(GPIO_PIN23, GPIO_PIN24, 9600);
-    sender_send_scancode(TEST_BYTE);
+    gpio_write(GPIO_PIN23, 0);
+    timer_delay_ms(230);
+    gpio_write(GPIO_PIN24, 1);
+    /*
+    sender_send_code(SETUP);
+    sender_send_code(PLAY);
+    sender_send_code(STOP);    
+    */
 }
     
