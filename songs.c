606fb0299f827885da1e982805a1a7d7536e854c
Genevieve Singer <gsinger@stanford.edu>
2018-06-06 19:20:39 -0700

    songs.c

--- songs.c
+++ songs.c
@@ -1,4 +1,7 @@
+#include "songs.h"
 //struct includes: char * artist, char * title, and a char * lyrics
+
+char * titles;
 
 struct song_t 
 {
@@ -7,3 +10,22 @@ struct song_t
     char * lyrics;
 };
 
+char * songs_init(void) 
+{
+    char * titles;
+    for (int i = 0; i < NUM_SONGS; i++) {
+        
+    }
+}
+
+song_t * load_songs(void) 
+{
+    song_t * songs;
+    for (int i = 0; i < NUM_SONGS; i++) {
+        song_t new_song;
+        
+        new_song.title = //first line in .txt file
+        new_song.artist = //second line in .txt file
+        new_song.lyrics = //loop through the rest 
+    }
+}
