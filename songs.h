edbf63cd5b55dbc87e760085aed8cfd973727c66
Genevieve Singer <gsinger@stanford.edu>
2018-06-06 19:30:36 -0700

    Song title edit

--- songs.h
+++ songs.h
@@ -4,10 +4,10 @@ const char * titles[NUM_SONGS];
 
 void load_titles() 
 {
-    titles[0] = "After the Storm";
-    titles[1] = "Boo'd Up";
-    titles[2] = "In The Morning";
-    titles[3] = "Season 2 Episode 3";
+    titles[0] = "After_the_Storm";
+    titles[1] = "Boo'd_Up";
+    titles[2] = "In_The_Morning";
+    titles[3] = "Season_2_Episode_3";
     titles[4] = "Honey";
     titles[5] = "Total";
 }
